3e8cc3c8773e6bae45542fabecb016a3ef48362a
kuragiia <gmerville1@gmail.com>
2023-10-10 21:17:50 +0100

    Added GetMapping controller

--- src/main/java/com/example/javacourses/appuser/interfaces/AppUserRepository.java
+++ src/main/java/com/example/javacourses/appuser/interfaces/AppUserRepository.java
@@ -2,6 +2,8 @@ package com.example.javacourses.appuser.interfaces;
 
 import com.example.javacourses.appuser.entities.AppUser;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import javax.transaction.Transactional;
@@ -12,4 +14,10 @@ import java.util.Optional;
 public interface AppUserRepository extends JpaRepository<AppUser, Long> {
 
     Optional<AppUser> findByEmail(String email);
+
+    @Transactional
+    @Modifying
+    @Query("UPDATE AppUser a " +
+            "SET a.enabled = TRUE WHERE a.email = ?1")
+    int enableAppUser(String email);
 }
--- src/main/java/com/example/javacourses/registration/services/RegistrationService.java
+++ src/main/java/com/example/javacourses/registration/services/RegistrationService.java
@@ -5,8 +5,13 @@ import com.example.javacourses.appuser.enums.AppUserRole;
 import com.example.javacourses.appuser.services.AppUserService;
 import com.example.javacourses.registration.EmailValidator;
 import com.example.javacourses.registration.RegistrationRequest;
+import com.example.javacourses.registration.token.ConfirmationToken;
+import com.example.javacourses.registration.token.ConfirmationTokenService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.time.LocalDateTime;
 
 @Service
 @AllArgsConstructor
@@ -14,6 +19,7 @@ public class RegistrationService {
 
     private final AppUserService appUserService;
     private final EmailValidator emailValidator;
+    private final ConfirmationTokenService confirmationTokenService;
 
     public String register(RegistrationRequest request) {
         boolean isValidEmail = emailValidator.
@@ -30,5 +36,28 @@ public class RegistrationService {
                         AppUserRole.USER
                 )
         );
+    }
+
+    @Transactional
+    public String confirmToken(String token) {
+        ConfirmationToken confirmationToken = confirmationTokenService
+                .getToken(token)
+                .orElseThrow(() ->
+                        new IllegalStateException("token not found"));
+
+        if (confirmationToken.getConfirmedAt() != null) {
+            throw new IllegalStateException("email already confirmed");
+        }
+
+        LocalDateTime expiredAt = confirmationToken.getExpiresAt();
+
+        if (expiredAt.isBefore(LocalDateTime.now())) {
+            throw new IllegalStateException("token expired");
+        }
+
+        confirmationTokenService.setConfirmedAt(token);
+        appUserService.enableAppUser(
+                confirmationToken.getAppUser().getEmail());
+        return "confirmed";
     }
 }
